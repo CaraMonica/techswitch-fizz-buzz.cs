e74275bfbb51e4cea59a58d079fd967d8bf6f691
caramonica <caramonica@hotmail.co.uk>
2021-09-20 16:20:28 +0100

    Working with user input and all rules.

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -8,30 +8,55 @@ namespace techswitch_fizz_buzz.cs
     class Program
     {
 
-        static void FizzBuzz1()
+        static int GetNumber(string message)
         {
-            for (int i = 1; i <= 100; i++)
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static IEnumerable<int> GetRules()
+        {
+            Console.WriteLine("Please select rules:");
+            Console.WriteLine(string.Join(Environment.NewLine, ruleDict));
+            Console.WriteLine("Type 'stop' when you're finished");
+
+            List<int> rules = new List<int>();
+
+            Console.WriteLine(ruleDict.Count);
+            while (rules.Count < ruleDict.Count)
             {
-                if (!IsDivisible(i, 3) && !IsDivisible(i, 5))
+                Console.WriteLine(rules.Count);
+                string rule = Console.ReadLine();
+
+                if (rule.Equals("stop"))
                 {
-                    Console.WriteLine(i);
-                    continue;
-                }
-                else if (IsDivisible(i, 3))
-                {
-                    Console.WriteLine("Fizz");
-                    continue;
-                }
-                else if (IsDivisible(i, 5))
-                {
-                    Console.WriteLine("Buzz");
-                    continue;
+                    if (rules.Count == 0)
+                    {
+                        Console.WriteLine("Must have at least one rule.");
+                    }
+                    else
+                    {
+                        break;
+                    }
+
                 }
                 else
                 {
-                    Console.WriteLine("FizzBuzz");
+                    int ruleNumber;
+
+                    if (int.TryParse(rule, out ruleNumber) && ruleDict.ContainsKey(ruleNumber) && !rules.Contains(ruleNumber))
+                    {
+                        rules.Add(ruleNumber);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a valid command.");
+                    }
                 }
             }
+
+            return rules.AsEnumerable();
+
         }
 
         static Dictionary<int, string> ruleDict = new Dictionary<int, string>()
@@ -44,23 +69,58 @@ namespace techswitch_fizz_buzz.cs
             {17,"reverse"},
         };
 
+        static int[] SPECIAL_RULES = new int[] { 11, 13, 17 };
+
         static bool IsDivisible(int number, int factor) => number % factor == 0;
 
-        static string GetFizzBuzzString(int number)
+        static string GetFizzBuzzString(int number, IEnumerable<int> rules)
         {
-            List<int> result = new List<int>();
-            return String.Join("", result);
+            List<string> result = new List<string>();
+
+            if (rules.Contains(11) && IsDivisible(number, 11))
+            {
+                rules = rules.Where(x => SPECIAL_RULES.Contains(x));
+            }
+
+            if (rules.Contains(17) && IsDivisible(number, 17))
+            {
+                rules = rules.Take(rules.Count() - 1).Reverse();
+            }
+
+            foreach (int rule in rules)
+            {
+                if (IsDivisible(number, rule))
+                {
+                    if (rule == 13)
+                    {
+                        int indexOfFirstB = result.FindIndex(word => word[0].Equals('B'));
+                        int index = indexOfFirstB == -1 ? 0 : indexOfFirstB;
+                        result.Insert(index, ruleDict[rule]);
+                    }
+                    else
+                    {
+                        result.Add(ruleDict[rule]);
+                    }
+                }
+            }
+            return result.Count() == 0 ? number.ToString() : String.Join("", result);
         }
 
-        static IEnumerable<int> FizzBuzz(int start, int end)
+        static void FizzBuzz(int start, int end, IEnumerable<int> rules)
         {
-            return Enumerable.Range(start, end).Select(x => x);
+            for (int i = start; i < end; i++)
+            {
+                Console.WriteLine(GetFizzBuzzString(i, rules));
+
+            }
         }
 
         static void Main(string[] args)
         {
-            var lines = ruleDict.Select(kvp => kvp.Key + ": " + kvp.Value.ToString());
-            Console.WriteLine(String.Join(Environment.NewLine, lines));
+            int start = GetNumber("Start number:");
+            int end = GetNumber("End number:");
+            IEnumerable<int> rules = GetRules();
+            FizzBuzz(start, end, rules);
         }
     }
 }
