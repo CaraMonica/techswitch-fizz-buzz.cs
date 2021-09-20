126b55ab1a7defc37b94e28094c5edd84e108149
caramonica <caramonica@hotmail.co.uk>
2021-09-20 10:42:18 +0100

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace techswitch_fizz_buzz.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
